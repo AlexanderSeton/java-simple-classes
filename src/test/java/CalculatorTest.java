a561128dd00da33780a555fe31cbb669f8e89809
Alexander Seton <alexanderseton@protonmail.com>
2022-01-04 15:37:14 +0000

    MVP: Calculator completed

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class CalculatorTest {
+
+    Calculator testCalculator;
+
+    @Before
+    public void before() {
+        testCalculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        int output = testCalculator.add(5, 10);
+        assertEquals(15, output);
+    }
+
+    @Test
+    public void canSubtract() {
+        int output = testCalculator.subtract(5, 10);
+        assertEquals(-5, output);
+    }
+
+    @Test
+    public void canMultiply() {
+        int output = testCalculator.multiply(500, 2);
+        assertEquals(1000, output);
+    }
+
+    @Test
+    public void canDivide() {
+        double output = testCalculator.divide(10.0, 3.3);
+        assertEquals(3.03, output, 0.01);
+    }
+
 }
